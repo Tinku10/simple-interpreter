8e8a0498089a0090e757a018851344b7a26baef6
Tinku Kalita <tinku.kvs@gmail.com>
2022-05-07 18:03:57 +0530

    Add actual callstack

--- src/CallStack.cpp
+++ src/CallStack.cpp
@@ -1,1 +1,44 @@
 #include "CallStack.h"
+
+ActivationRecords::ActivationRecords(const std::string& name,
+                                     ActivationRecordType type,
+                                     uint level)
+    : name(name), type(type), level(level) {
+}
+
+void ActivationRecords::add(std::string& name, int value) {
+  members[name] = value;
+}
+
+int ActivationRecords::at(std::string& name) {
+  if (members.count(name)) return members.at(name);
+
+  throw error(ErrorCode::IDENTIFIER_NOT_FOUND);
+}
+
+Exception ActivationRecords::error(ErrorCode code) {
+  std::string msg = "";
+  switch (code) {
+    case ErrorCode::IDENTIFIER_NOT_FOUND:
+      msg = "undeclared identifier\n";
+      break;
+    default: msg = "unknown error\n";
+  }
+
+  throw Exception(code, msg);
+}
+
+CallStack::CallStack() {
+}
+
+void CallStack::push(std::unique_ptr<ActivationRecords> stack_frame) {
+  stack.push(std::move(stack_frame));
+}
+
+void CallStack::pop() {
+  stack.pop();
+}
+
+std::unique_ptr<ActivationRecords>& CallStack::top() {
+  return stack.top();
+}
--- src/CallStack.h
+++ src/CallStack.h
@@ -1,5 +1,40 @@
+#pragma once
+
+#include <iostream>
 #include <map>
+#include <stack>
+
+#include "Exceptions.h"
+#include "Token.h"
+
+enum struct ActivationRecordType { PROGRAM, PROCEDURE };
+
+class ActivationRecords {
+ public:
+  std::string name;
+  ActivationRecordType type;
+  uint level;
+  std::map<std::string, int> members;
+
+  ActivationRecords(const std::string& name,
+                    ActivationRecordType type,
+                    uint level);
+
+  void add(std::string& name, int value);
+
+  int at(std::string& name);
+
+  Exception error(ErrorCode code);
+};
+
 class CallStack {
-public:
+ public:
   std::map<std::string, int> cache;
+  std::stack<std::unique_ptr<ActivationRecords>> stack;
+
+  CallStack();
+
+  void push(std::unique_ptr<ActivationRecords> stack_frame);
+  void pop();
+  std::unique_ptr<ActivationRecords>& top();
 };
